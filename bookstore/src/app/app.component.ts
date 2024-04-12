6560cd72d51087c2f1ba5403f6867a7a1b5dfb5d
Elmar Putz <ep@studiomitte.com>
2024-04-12 16:38:22 +0200

    routing done

--- bookstore/src/app/app.component.ts
+++ bookstore/src/app/app.component.ts
@@ -1,32 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { BookListComponent } from './book-list/book-list.component';
-import { BookDetailsComponent } from './book-details/book-details.component';
-import { Book } from './shared/book';
-
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'bs-root',
   standalone: true,
-  imports: [RouterOutlet,BookListComponent,BookDetailsComponent],
+  imports: [RouterOutlet,RouterLink,RouterLinkActive],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'bookstore';
-  listOn = true;
-  detailsOn = false; 
-
-  book: Book | undefined;
-
-  showList() {
-    this.listOn = true;
-    this.detailsOn = false;
-  }
-
-  showDetails(book: Book) {
-    this.book = book;
-    this.listOn = false;
-    this.detailsOn = true;
-  }
 
 }
--- bookstore/src/app/book-details/book-details.component.ts
+++ bookstore/src/app/book-details/book-details.component.ts
@@ -1,24 +1,33 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'bs-book-details',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './book-details.component.html',
   styles: ``
 })
 export class BookDetailsComponent {
 
-  @Input() book: Book | undefined;
-  @Output() showListEvent = new EventEmitter<any>();
+  book: Book | undefined;
+
+  constructor (
+    private bs: BookStoreService, 
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    const params = this.route.snapshot.params;
+    this.book = this.bs.getSingle(params['isbn']);
+  }
 
   getRating(num: number) {
     return new Array(num);
   }
 
-  showBookList() {
-    this.showListEvent.emit();
-  }
+
 
 }
